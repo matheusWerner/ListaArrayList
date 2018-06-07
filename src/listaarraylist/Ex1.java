575951b15f17c6e86098f1fc65be7408a6201a5b
matheus ruan werner <matheus.lol45@gmail.com>
2018-06-07 09:54:52 -0300

    Initial commit

---
+++ src/listaarraylist/Ex1.java
@@ -0,0 +1,24 @@
+
+package listaarraylist;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Ex1 {
+
+  
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> numeros = new ArrayList<>();
+        
+        for (int i = 0; i <= 15; i++) {
+            numeros.add(Integer.parseInt(JOptionPane.showInputDialog("Digite um número")));
+        }
+        
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.println(numeros.get(i));
+        }
+    }
+    
+}
+
