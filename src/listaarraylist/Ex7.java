361079750b907d18accfc66f368d25fe063a9895
matheus ruan werner <matheus.lol45@gmail.com>
2018-06-14 11:30:43 -0300

    ArrayList

--- src/listaarraylist/Ex7.java
+++ src/listaarraylist/Ex7.java
@@ -19,26 +19,60 @@ public class Ex7 {
         ArrayList<String> nomes = new ArrayList<>();
         
         
-        for(int i = 0; i < 25; i++) {
+        for(int i = 0; i < 5; i++) {
             nomes.add(JOptionPane.showInputDialog("Digite um nome"));
         }
         
         
-        String totalNomes = "";
+       String totalNomes = "";
         for (int i = 0; i < nomes.size(); i++) {
-            totalNomes = nomes.get(i) + "\n";
+            totalNomes += nomes.get(i) + "\n";
+            
         }
         JOptionPane.showMessageDialog(null, totalNomes);
         
         
+        
         String maiorNome = "";
         for (int i = 0; i < nomes.size(); i++) {
-            
-            
-            
-           
+           if(nomes.get(i).length() > maiorNome.length()) {
+               maiorNome = nomes.get(i);
+           }          
+        }
+        JOptionPane.showMessageDialog(null, maiorNome);
+        
+        String menorNome = "";
+        for (int i = 0; i < nomes.size(); i++) {
+             if(nomes.get(i).length() > menorNome.length()) {
+               menorNome = nomes.get(i);
+           }
+        } 
+            for (int j = 0; j < nomes.size(); j++){
+               if(nomes.get(j).length() < menorNome.length()) {
+               menorNome = nomes.get(j);
+           }  
         }
         
+        JOptionPane.showMessageDialog(null, menorNome);
+        
+        int quantPaulo = 0;
+        
+        for (int i = 0; i < nomes.size(); i++) {
+            if (nomes.get(i).contains("Paulo")) {
+                quantPaulo++;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Quantidad de Paulo é: " + quantPaulo);
+        
+        int quantAna = 0;
+        
+        for (int i = 0; i < nomes.size(); i++) {
+            if (nomes.get(i).contains("Ana")) {
+                quantPaulo++;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Quantidad de Paulo é: " + quantAna);
+       
         
     }
     
